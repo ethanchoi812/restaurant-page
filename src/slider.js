4a993803eaccaad625477b358a6981ff983419c1
Ethan Choi <ethanchoi.812@gmail.com>
2019-09-19 14:56:15 +0800

    Add id to images

--- src/slider.js
+++ src/slider.js
@@ -6,9 +6,9 @@ const slider = () => {
 
     let buttons = "<span class=\"nav-btn\" id=\"nav-btn-left\">&lt;</span><span class=\"nav-btn\" id=\"nav-btn-right\">&gt;</span>";
     
-    let img1 = "<img src=\"https://via.placeholder.com/1280x384/0000FF\">";
-    let img2 = "<img src=\"https://via.placeholder.com/1280x384/FF0000\">";
-    let img3 = "<img src=\"https://via.placeholder.com/1280x384/FFFF00\">";
+    let img1 = "<img id=\"slide-1\" src=\"https://via.placeholder.com/1280x384/0000FF\">";
+    let img2 = "<img id=\"slide-2\" src=\"https://via.placeholder.com/1280x384/FF0000\">";
+    let img3 = "<img id=\"slide-3\" src=\"https://via.placeholder.com/1280x384/FFFF00\">";
 
     element.innerHTML = buttons + "<div id=\"image-container\">" + img1 + img2 + img3 + "</div>";
 
