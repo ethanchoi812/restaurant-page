342594fb01baeb964c29a9582a30c6e10620515e
Ethan Choi <ethanchoi.812@gmail.com>
2019-09-22 17:19:15 +0800

    Add Prettier

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
-import pageLoad from './pageLoad';
+import pageLoad from "./pageLoad";
 import navigation from "./navigation";
 
 document.body.appendChild(pageLoad());
 
-const tabDiv = document.createElement('div');
+const tabDiv = document.createElement("div");
 
-let menu = document.getElementById('menu');
-let contact = document.getElementById('contact');
+let menu = document.getElementById("menu");
+let contact = document.getElementById("contact");
 
-menu.onclick = function(e){showContent('menu')};
-contact.onclick = function(e){showContent('contact')};
+menu.onclick = function(e) {
+  showContent("menu");
+};
+contact.onclick = function(e) {
+  showContent("contact");
+};
 
 function showContent(tab) {
+  let tabContent;
 
-    let tabContent;
+  if (tab === "menu") {
+    tabContent = "This is my menu";
+  } else if (tab === "contact") {
+    tabContent = "Contact here";
+  } else {
+    tabContent = "";
+  }
 
-    if(tab === 'menu'){
-        tabContent = 'This is my menu';
-    } else if (tab === 'contact') {
-        tabContent = 'Contact here';
-    } else {
-        tabContent = '';
-    }
-
-    tabDiv.innerHTML = tabContent;
-    return tabDiv;
+  tabDiv.innerHTML = tabContent;
+  return tabDiv;
 }
 
 document.body.appendChild(tabDiv);
 
 navigation();
-
