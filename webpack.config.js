304a262037986477264461e12bf117814175acdf
Ethan Choi <ethanchoi.812@gmail.com>
2019-10-06 15:48:06 +0800

    Try/remove font loading

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
       }
     ]
   }
