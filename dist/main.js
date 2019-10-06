b59196ccdbd974597a8e4c09d3314c50f1ea8494
Ethan Choi <ethanchoi.812@gmail.com>
2019-10-06 17:08:25 +0800

    Add babel

--- dist/main.js
+++ dist/main.js
@@ -92,11 +92,11 @@
               if (e && "function" == typeof btoa) {
                 var i =
                     ((o = r),
-                    (s = btoa(unescape(encodeURIComponent(JSON.stringify(o))))),
-                    (c = "sourceMappingURL=data:application/json;charset=utf-8;base64,".concat(
-                      s
+                    (c = btoa(unescape(encodeURIComponent(JSON.stringify(o))))),
+                    (s = "sourceMappingURL=data:application/json;charset=utf-8;base64,".concat(
+                      c
                     )),
-                    "/*# ".concat(c, " */")),
+                    "/*# ".concat(s, " */")),
                   a = r.sources.map(function(n) {
                     return "/*# sourceURL="
                       .concat(r.sourceRoot)
@@ -107,7 +107,7 @@
                   .concat([i])
                   .join("\n");
               }
-              var o, s, c;
+              var o, c, s;
               return [t].join("\n");
             })(e, n);
             return e[2] ? "@media ".concat(e[2], "{").concat(t, "}") : t;
@@ -120,12 +120,12 @@
             null != a && (r[a] = !0);
           }
           for (var o = 0; o < n.length; o++) {
-            var s = n[o];
-            (null != s[0] && r[s[0]]) ||
-              (t && !s[2]
-                ? (s[2] = t)
-                : t && (s[2] = "(".concat(s[2], ") and (").concat(t, ")")),
-              e.push(s));
+            var c = n[o];
+            (null != c[0] && r[c[0]]) ||
+              (t && !c[2]
+                ? (c[2] = t)
+                : t && (c[2] = "(".concat(c[2], ") and (").concat(t, ")")),
+              e.push(c));
           }
         }),
         e
@@ -162,30 +162,30 @@
           return n[e];
         };
       })();
-    function s(n, e) {
+    function c(n, e) {
       for (var t = [], r = {}, i = 0; i < n.length; i++) {
         var a = n[i],
           o = e.base ? a[0] + e.base : a[0],
-          s = { css: a[1], media: a[2], sourceMap: a[3] };
-        r[o] ? r[o].parts.push(s) : t.push((r[o] = { id: o, parts: [s] }));
+          c = { css: a[1], media: a[2], sourceMap: a[3] };
+        r[o] ? r[o].parts.push(c) : t.push((r[o] = { id: o, parts: [c] }));
       }
       return t;
     }
-    function c(n, e) {
+    function s(n, e) {
       for (var t = 0; t < n.length; t++) {
         var r = n[t],
           a = i[r.id],
           o = 0;
         if (a) {
           for (a.refs++; o < a.parts.length; o++) a.parts[o](r.parts[o]);
-          for (; o < r.parts.length; o++) a.parts.push(m(r.parts[o], e));
+          for (; o < r.parts.length; o++) a.parts.push(h(r.parts[o], e));
         } else {
-          for (var s = []; o < r.parts.length; o++) s.push(m(r.parts[o], e));
-          i[r.id] = { id: r.id, refs: 1, parts: s };
+          for (var c = []; o < r.parts.length; o++) c.push(h(r.parts[o], e));
+          i[r.id] = { id: r.id, refs: 1, parts: c };
         }
       }
     }
-    function d(n) {
+    function u(n) {
       var e = document.createElement("style");
       if (void 0 === n.attributes.nonce) {
         var r = t.nc;
@@ -208,15 +208,15 @@
       }
       return e;
     }
-    var l,
-      u =
-        ((l = []),
+    var d,
+      l =
+        ((d = []),
         function(n, e) {
-          return (l[n] = e), l.filter(Boolean).join("\n");
+          return (d[n] = e), d.filter(Boolean).join("\n");
         });
     function f(n, e, t, r) {
       var i = t ? "" : r.css;
-      if (n.styleSheet) n.styleSheet.cssText = u(e, i);
+      if (n.styleSheet) n.styleSheet.cssText = l(e, i);
       else {
         var a = document.createTextNode(i),
           o = n.childNodes;
@@ -224,37 +224,38 @@
           o.length ? n.insertBefore(a, o[e]) : n.appendChild(a);
       }
     }
-    var p = null,
-      v = 0;
-    function m(n, e) {
+    function p(n, e, t) {
+      var r = t.css,
+        i = t.media,
+        a = t.sourceMap;
+      if (
+        (i && n.setAttribute("media", i),
+        a &&
+          btoa &&
+          (r += "\n/*# sourceMappingURL=data:application/json;base64,".concat(
+            btoa(unescape(encodeURIComponent(JSON.stringify(a)))),
+            " */"
+          )),
+        n.styleSheet)
+      )
+        n.styleSheet.cssText = r;
+      else {
+        for (; n.firstChild; ) n.removeChild(n.firstChild);
+        n.appendChild(document.createTextNode(r));
+      }
+    }
+    var v = null,
+      m = 0;
+    function h(n, e) {
       var t, r, i;
       if (e.singleton) {
-        var a = v++;
-        (t = p || (p = d(e))),
+        var a = m++;
+        (t = v || (v = u(e))),
           (r = f.bind(null, t, a, !1)),
           (i = f.bind(null, t, a, !0));
       } else
-        (t = d(e)),
-          (r = function(n, e, t) {
-            var r = t.css,
-              i = t.media,
-              a = t.sourceMap;
-            if (
-              (i && n.setAttribute("media", i),
-              a &&
-                btoa &&
-                (r += "\n/*# sourceMappingURL=data:application/json;base64,".concat(
-                  btoa(unescape(encodeURIComponent(JSON.stringify(a)))),
-                  " */"
-                )),
-              n.styleSheet)
-            )
-              n.styleSheet.cssText = r;
-            else {
-              for (; n.firstChild; ) n.removeChild(n.firstChild);
-              n.appendChild(document.createTextNode(r));
-            }
-          }.bind(null, t, e)),
+        (t = u(e)),
+          (r = p.bind(null, t, e)),
           (i = function() {
             !(function(n) {
               if (null === n.parentNode) return !1;
@@ -280,21 +281,21 @@
       ((e = e || {}).attributes =
         "object" == typeof e.attributes ? e.attributes : {}),
         e.singleton || "boolean" == typeof e.singleton || (e.singleton = a());
-      var t = s(n, e);
+      var t = c(n, e);
       return (
-        c(t, e),
+        s(t, e),
         function(n) {
           for (var r = [], a = 0; a < t.length; a++) {
             var o = t[a],
-              d = i[o.id];
-            d && (d.refs--, r.push(d));
+              u = i[o.id];
+            u && (u.refs--, r.push(u));
           }
-          n && c(s(n, e), e);
-          for (var l = 0; l < r.length; l++) {
-            var u = r[l];
-            if (0 === u.refs) {
-              for (var f = 0; f < u.parts.length; f++) u.parts[f]();
-              delete i[u.id];
+          n && s(c(n, e), e);
+          for (var d = 0; d < r.length; d++) {
+            var l = r[d];
+            if (0 === l.refs) {
+              for (var f = 0; f < l.parts.length; f++) l.parts[f]();
+              delete i[l.id];
             }
           }
         }
@@ -309,103 +310,100 @@
       i = t.n(r),
       a = t(1),
       o = t.n(a),
-      s = t(2),
-      c = t.n(s),
-      d = t(3),
-      l = t.n(d);
-    const u = n => {
-      let e = new Image();
-      return (e.src = n), e;
-    };
-    var f = () => {
-      const n = u(i.a),
-        e = u(o.a),
-        t = u(c.a),
-        r = u(l.a);
-      let a = document.createElement("div");
-      a.setAttribute("id", "image-container"),
-        (a.innerHTML = n.outerHTML + e.outerHTML + t.outerHTML + r.outerHTML);
-      let s = document.createElement("div");
-      s.className = "slider-div";
-      (s.innerHTML +=
-        '<span class="nav-btn" id="nav-btn-left">&lt;</span><span class="nav-btn" id="nav-btn-right">&gt;</span>'),
-        s.appendChild(a);
-      return (
-        (s.innerHTML +=
-          '<div class="slide-marker"><span id="slide-marker-0" data-slide-index="0"></span><span id="slide-marker-1" data-slide-index="1"></span><span id="slide-marker-2" data-slide-index="2"></span><span id="slide-marker-3" data-slide-index="3"></span></div>'),
-        s
-      );
-    };
-    var p = () => {
-      let n = document.createElement("div");
-      n.classList.add("main");
-      return (
-        (n.innerHTML =
-          '<h1>The City Restaurant</h1><div><ul id="nav"><li id="menu">Menu</li><li id="contact">Contact</li></ul></div>'),
-        n.appendChild(f()),
-        (n.innerHTML += '<div id="pageContent"></div>'),
-        n
-      );
-    };
-    var v = n => {
-      let e = document.querySelector(".slider-div").offsetWidth;
-      document.getElementById(
-        "image-container"
-      ).style.transform = `translate(-${n * e}px)`;
-      let t = document.querySelectorAll(".slide-marker span"),
-        r = document.getElementById(`slide-marker-${n}`);
-      t.forEach(n => {
-        n.style.background = "#ccc";
-      }),
-        (r.style.background = "#fff");
-    };
-    var m = n => {
-      let e,
-        t = document.getElementById("nav-btn-left"),
-        r = document.getElementById("nav-btn-right"),
-        i = n;
-      const a = () => {
-          e = setInterval(
-            () => (i < 4 && (v(i), i++), 4 === i && (i = 0), i),
-            4e3
-          );
-        },
-        o = () => {
-          clearInterval(e);
+      c = t(2),
+      s = t.n(c),
+      u = t(3),
+      d = t.n(u),
+      l = function(n) {
+        var e = new Image();
+        return (e.src = n), e;
+      },
+      f = function() {
+        var n = l(i.a),
+          e = l(o.a),
+          t = l(s.a),
+          r = l(d.a),
+          a = document.createElement("div");
+        a.setAttribute("id", "image-container"),
+          (a.innerHTML = n.outerHTML + e.outerHTML + t.outerHTML + r.outerHTML);
+        var c = document.createElement("div");
+        c.className = "slider-div";
+        (c.innerHTML +=
+          '<span class="nav-btn" id="nav-btn-left">&lt;</span><span class="nav-btn" id="nav-btn-right">&gt;</span>'),
+          c.appendChild(a);
+        return (
+          (c.innerHTML +=
+            '<div class="slide-marker"><span id="slide-marker-0" data-slide-index="0"></span><span id="slide-marker-1" data-slide-index="1"></span><span id="slide-marker-2" data-slide-index="2"></span><span id="slide-marker-3" data-slide-index="3"></span></div>'),
+          c
+        );
+      },
+      p = function() {
+        var n = document.createElement("div");
+        n.classList.add("main");
+        return (
+          (n.innerHTML =
+            '<h1>The City Restaurant</h1><div><ul id="nav"><li id="menu">Menu</li><li id="contact">Contact</li></ul></div>'),
+          n.appendChild(f()),
+          (n.innerHTML += '<div id="pageContent"></div>'),
+          n
+        );
+      },
+      v = function(n) {
+        var e = document.querySelector(".slider-div").offsetWidth;
+        document.getElementById(
+          "image-container"
+        ).style.transform = "translate(-".concat(n * e, "px)");
+        var t = document.querySelectorAll(".slide-marker span"),
+          r = document.getElementById("slide-marker-".concat(n));
+        t.forEach(function(n) {
+          n.style.background = "#ccc";
+        }),
+          (r.style.background = "#fff");
+      },
+      m = function(n) {
+        var e,
+          t = document.getElementById("nav-btn-left"),
+          r = document.getElementById("nav-btn-right"),
+          i = n,
+          a = function() {
+            e = setInterval(function() {
+              return i < 4 && (v(i), i++), 4 === i && (i = 0), i;
+            }, 4e3);
+          },
+          o = function() {
+            clearInterval(e);
+          };
+        v(i),
+          a(),
+          t.addEventListener("click", function() {
+            return o(), i < 4 && i > 0 && v((i -= 1)), a(), i;
+          }),
+          r.addEventListener("click", function() {
+            return o(), i < 3 && i >= 0 && v((i += 1)), a(), i;
+          }),
+          document.querySelectorAll(".slide-marker span").forEach(function(n) {
+            n.addEventListener("click", function() {
+              o(), (i = Number(n.dataset.slideIndex)), v(i), a();
+            });
+          });
+      },
+      h = function() {
+        var n,
+          e = document.getElementById("pageContent"),
+          t = "Restaurant menu";
+        (e.innerHTML = t),
+          document.querySelectorAll("#nav li").forEach(function(e) {
+            e.addEventListener("click", function() {
+              (n = e.id), r(n);
+            });
+          });
+        var r = function(n) {
+          "menu" === n
+            ? (t = "Restaurant menu")
+            : "contact" === n && (t = "Contact here"),
+            (e.innerHTML = t);
         };
-      v(i),
-        a(),
-        t.addEventListener(
-          "click",
-          () => (o(), i < 4 && i > 0 && v((i -= 1)), a(), i)
-        ),
-        r.addEventListener(
-          "click",
-          () => (o(), i < 3 && i >= 0 && v((i += 1)), a(), i)
-        ),
-        document.querySelectorAll(".slide-marker span").forEach(n => {
-          n.addEventListener("click", () => {
-            o(), (i = Number(n.dataset.slideIndex)), v(i), a();
-          });
-        });
-    };
-    var h = () => {
-      let n,
-        e = document.getElementById("pageContent"),
-        t = "Restaurant menu";
-      (e.innerHTML = t),
-        document.querySelectorAll("#nav li").forEach(e => {
-          e.addEventListener("click", () => {
-            (n = e.id), r(n);
-          });
-        });
-      const r = n => {
-        "menu" === n
-          ? (t = "Restaurant menu")
-          : "contact" === n && (t = "Contact here"),
-          (e.innerHTML = t);
       };
-    };
     document.body.appendChild(p());
     m(0), h();
   }
